e9d190d5b6c0a2f4f1f1cae3a16b0f1fd0b091e6
Waleed ur Rehman <54763708+Waleed-Ur-Rehaman@users.noreply.github.com>
2021-04-18 14:30:29 +0500

    Initial commit

---
+++ todo/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('delete/<int:pk>/', views.delete, name="delete"),
+]
---
+++ todo/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+
+# Create your views here.
+
+
+def index(request):
+    todo = Todo.objects.all()
+    if request.method == 'POST':
+        new_todo = Todo(
+            title=request.POST['title']
+        )
+        new_todo.save()
+        return redirect('/')
+
+    return render(request, 'index.html', {'todos': todo})
+
+
+def delete(request, pk):
+    todos = Todo.objects.get(id=pk)
+    todos.delete()
+    return redirect('/')
